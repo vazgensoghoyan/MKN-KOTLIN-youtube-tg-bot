9df2c6bb56f3b983b9e5ae1ab6d55f0018d37413
Согоян Вазген <92378539+vazgensoghoyan@users.noreply.github.com>
2025-07-04 23:11:51 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 import bot.VideoInfoBot
 
-const val BOT_TOKEN = "7785348052:AAGC8R29NsNmH45QzkyazTaXE2BVZkKvq48"
-const val YT_TOKEN = "AIzaSyDZWPc8ZoH3qGMMCRbHQdZ5DuEByyp-Tfc"
+const val BOT_TOKEN = ""
+const val YT_TOKEN = ""
 
 suspend fun main() {
     val bot = VideoInfoBot(BOT_TOKEN, YT_TOKEN)
